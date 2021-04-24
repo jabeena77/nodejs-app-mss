d7ef7ff40e757aa5a2a139883416c197b8ba2d11
jabeena77 <80161390+jabeena77@users.noreply.github.com>
2021-04-24 16:14:49 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,13 +1,14 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://54.253.237.52:9000/',
+  serverUrl: 'http://13.233.120.222:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsMithunTechnologies',
-	    'sonar.login': 'admin',
-	    'sonar.password': 'admin',
+	    <!--'sonar.login': 'admin',
+	    'sonar.password': 'admin',-->
+	       <sonar.login>80ea5c229d15686e00896ecc6707ba2c2eb6adb9</sonar.login> 
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
             'sonar.sourceEncoding':'UTF-8',
